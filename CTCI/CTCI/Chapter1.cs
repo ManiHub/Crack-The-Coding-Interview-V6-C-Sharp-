5f94211281b863f27eeae03e5852f09d1b1d79f5
Manikanta Kotthapalli <k.manikanta33@hotmail.com>
2017-06-13 12:18:54 -0700

    Initial structure- added first 4 chapers info

---
+++ CTCI/CTCI/Chapter1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI
+{
+    /// <summary>
+    /// Problems on Strings and Arrays
+    /// </summary>
+    class Chapter1
+    {
+    }
+}
---
+++ CTCI/CTCI/Chapter4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI
+{
+    /// <summary>
+    /// Problems on Trees and Graphs
+    /// </summary>
+    class Chapter4
+    {
+    }
+}
